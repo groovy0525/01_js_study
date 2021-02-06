810eaebfb117f2dbcd4ff0a3e1970d6568ce9bbd
baby4 <vsakurav07@gmail.com>
2021-02-06 09:12:29 +0900

    feature: reset css 추가

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ResetCss from './shared/reset-css'
 
 import Webtoon from './webtoon'
 
 function App() {
   return (
     <>
+      <ResetCss />
       <Webtoon />
     </>
   )
